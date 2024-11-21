126868863d571a36d37739171d170fecec032e06
Morgan Polak <polakmaor@gmail.com>
2024-11-21 04:27:27 +0000

    ENH: updates the production socket.io configuration

--- src/socket.ts
+++ src/socket.ts
@@ -4,8 +4,10 @@ const isProduction = process.env.NODE_ENV === 'production';
 const socketUrl = isProduction ?
     process.env.REACT_APP_BASE_API_URL_PROD :
     process.env.REACT_APP_BASE_API_URL_DEV;
+const socketPath = isProduction ? '/websocket/socket.io' : '';
 
 export const socket = io(socketUrl, {
+    path: socketPath,
     transports: ['websocket'],
     secure: isProduction,
     rejectUnauthorized: false,
